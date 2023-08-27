e38bc4a58fab1a2bdb5790ce6ac9e21cd346c693
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-08-27 16:11:30 +0530

    Temperature

---
+++ 2. Input, Output and Datatypes/Temperature.java
@@ -0,0 +1,17 @@
+package com.priyanshi;
+
+import java.util.Scanner;
+
+// 2. Convert temperature from Celsius to Fahrenheit
+
+public class Temperature {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter temperature in C: ");
+        float tempC = sc.nextFloat();
+        // temperature conversion
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+    }
+}
