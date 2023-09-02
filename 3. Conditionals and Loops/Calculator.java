c8abc4a27a65f3cd5382f1d699170a36b2c07cbd
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-09-02 23:46:46 +0530

    Calculator

---
+++ 3. Conditionals and Loops/Calculator.java
@@ -0,0 +1,47 @@
+package com.priyanshi;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Take input from user till user does not press X or x
+        int ans = 0;
+        while (true) {
+            // take the operator as input
+            System.out.print("Enter the operator: ");
+            char op = sc.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                // input two numbers
+                System.out.print("Enter two numbers: ");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                System.out.println("End of program.");
+                break;
+            } else {
+                System.out.println("Invalid operation!");
+            }
+            System.out.println(ans);
+        } // end of while loop
+    }
+}
