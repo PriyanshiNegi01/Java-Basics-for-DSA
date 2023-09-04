4614024dfcb3ed7a29498e08fe28a4d598d79faa
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-09-04 17:31:51 +0530

    Day of Week

---
+++ 4. Switch/DayOfWeek.java
@@ -0,0 +1,68 @@
+package com.priyanshi;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+        // Enhanced switch statement
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//        }
+
+        // For weekdays and weekends (benefit of not giving a break statement)
+//        switch (day) {
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Weekday");
+//                break;
+//            case 6:
+//            case 7:
+//                System.out.println("Weekend");
+//                break;
+//        }
+
+        // Enhanced switch statement
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+        }
+
+    }
+}
