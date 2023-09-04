c46b7c8184733ce51d59bf2637618cf4f7b1c875
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-09-04 17:32:25 +0530

    Nested Switch Statements

---
+++ 4. Switch/NestedSwitch.java
@@ -0,0 +1,51 @@
+package com.priyanshi;
+
+import java.util.Scanner;
+
+public class NestedSwitch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int empID = sc.nextInt();
+        String department = sc.next();
+
+        switch (empID) {
+            case 1:
+                System.out.println("Priyanshi Negi");
+                break;
+            case 2:
+                System.out.println("Isha Singh");
+                break;
+            case 3:
+                System.out.println("Emp Number 3");
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management Department");
+                        break;
+                    default:
+                        System.out.println("No department entered");
+                }
+                break;
+            default:
+                System.out.println("Enter correct empID");
+        }
+
+        // Enhanced Switch
+        switch (empID) {
+            case 1 -> System.out.println("Priyanshi Negi");
+            case 2 -> System.out.println("Isha Singh");
+            case 3 -> {
+                System.out.println("Emp Number 3");
+                switch (department) {
+                    case "IT" -> System.out.println("IT Department");
+                    case "Management" -> System.out.println("Management Department");
+                    default -> System.out.println("No department entered");
+                }
+            }
+            default -> System.out.println("Enter correct empID");
+        }
+    }
+}
