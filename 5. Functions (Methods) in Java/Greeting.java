d575d6ecd59aeddc12a80a9c8ae130a3a9f3ec2a
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-09-14 09:30:53 +0530

    Greeting file

---
+++ 5. Functions (Methods) in Java/Greeting.java
@@ -0,0 +1,11 @@
+package com.priyanshi;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
