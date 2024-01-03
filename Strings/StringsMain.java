8b5e4bf80305ddcbd8fcc6d7058119e4d4ff2148
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2024-01-03 14:24:51 +0530

    feat: Strings Main

---
+++ Strings/StringsMain.java
@@ -0,0 +1,16 @@
+package com.priyanshi;
+
+public class StringsMain {
+
+    public static void main(String[] args) {
+        int num = 12;
+        int[] arr = {2, 3, 5, 10, 21};
+        String name = "Priyanshi Negi";
+//        System.out.println(name);
+        String a = "Priyanshi";
+        System.out.println(a);
+        a = "Negi";
+        String b = "Priyanshi";
+        // Here, a and b are pointing to the same object
+    }
+}
