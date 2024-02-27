6b1e5c24b9f2870dfc321ac6ac86acaf32c31967
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2024-02-27 09:55:26 +0530

    feat: code files

---
+++ 2. Input and Output/CompoundInterest.java
@@ -0,0 +1,13 @@
+package com.priyanshi.InputOutput;
+
+// 6. Write a program to input principal, time, and rate (P, T, R) from the user and find Compound Interest
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
